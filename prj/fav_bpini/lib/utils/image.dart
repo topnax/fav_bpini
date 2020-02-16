a6cfadddb84230d445d41361e59b4800d98d1d7e
Stanislav Král <stanislav.kral3@gmail.com>
2020-02-16 23:50:09 +0100

    Added image transf. methods

--- prj/fav_bpini/lib/utils/image.dart
+++ prj/fav_bpini/lib/utils/image.dart
@@ -15,17 +15,14 @@ imglib.Image convertCameraImage(CameraImage image) {
   final int uvPixelStride = image.planes[1].bytesPerPixel;
   for (int x = 0; x < width; x++) {
     for (int y = 0; y < height; y++) {
-      final int uvIndex =
-          uvPixelStride * (x / 2).floor() + uvyButtonStride * (y / 2).floor();
+      final int uvIndex = uvPixelStride * (x / 2).floor() + uvyButtonStride * (y / 2).floor();
       final int index = y * width + x;
       final yp = image.planes[0].bytes[index];
       final up = image.planes[1].bytes[uvIndex];
       final vp = image.planes[2].bytes[uvIndex];
 // Calculate pixel color
       int r = (yp + vp * 1436 / 1024 - 179).round().clamp(0, 255);
-      int g = (yp - up * 46549 / 131072 + 44 - vp * 93604 / 131072 + 91)
-          .round()
-          .clamp(0, 255);
+      int g = (yp - up * 46549 / 131072 + 44 - vp * 93604 / 131072 + 91).round().clamp(0, 255);
       int b = (yp + up * 1814 / 1024 - 227).round().clamp(0, 255);
 // color: 0x FF  FF  FF  FF
 //           A   B   G   R
@@ -40,12 +37,8 @@ imglib.Image convertCameraImage(CameraImage image) {
 double getWhiteToBlackRatio(Rect boundingBox, imglib.Image img) {
   int total = 0;
   int white = 0;
-  for (int i = max(boundingBox.top.toInt(), 0);
-      i < min(boundingBox.bottom.toInt(), img.height);
-      i++) {
-    for (int j = max(boundingBox.left.toInt(), 0);
-        j < min(boundingBox.right.toInt(), img.width);
-        j++) {
+  for (int i = max(boundingBox.top.toInt(), 0); i < min(boundingBox.bottom.toInt(), img.height); i++) {
+    for (int j = max(boundingBox.left.toInt(), 0); j < min(boundingBox.right.toInt(), img.width); j++) {
       var color = img.getPixel(j, i);
       int r = (color & 0xFF);
       int g = ((color >> 8) & 0xFF);
@@ -57,51 +50,86 @@ double getWhiteToBlackRatio(Rect boundingBox, imglib.Image img) {
   }
 
   return (white.toDouble() / total.toDouble());
-
-//  for (var textBlock in detectedBlocks) {
-//    int total = 0;
-//    int white = 0;
-//    for (int i = max(textBlock.boundingBox.top.toInt(), 0);
-//    i < min(textBlock.boundingBox.bottom.toInt(), img.height);
-//    i++) {
-//      for (int j = max(textBlock.boundingBox.left.toInt(), 0);
-//      j < min(textBlock.boundingBox.right.toInt(), img.width);
-//      j++) {
-//        var color = img.getPixel(j, i);
-//        int r = (color & 0xFF);
-//        int g = ((color >> 8) & 0xFF);
-//        int b = ((color >> 16) & 0xFF);
-//        var y = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-//        white += y < 128 ? 0 : 1;
-//        total++;
-//      }
-//    }
-//    debugPrint(textBlock.text +
-//        " - ratio " +
-//        (white.toDouble() / total.toDouble()).toString());
-//  }
 }
 
-
-int getBht(Rect boundingBox, imglib.Image img) {
-
+imglib.Image convertImageToGrayScale(imglib.Image img) {
   int total = 0;
   int white = 0;
-  for (int i = max(boundingBox.top.toInt(), 0);
-  i < min(boundingBox.bottom.toInt(), img.height);
-  i++) {
-    for (int j = max(boundingBox.left.toInt(), 0);
-    j < min(boundingBox.right.toInt(), img.width);
-    j++) {
+  for (int i = 0; i < img.height; i++) {
+    for (int j = 0; j < img.width; j++) {
       var color = img.getPixel(j, i);
       int r = (color & 0xFF);
       int g = ((color >> 8) & 0xFF);
       int b = ((color >> 16) & 0xFF);
-      var y = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-      white += y < 200 ? 0 : 1;
-      total++;
+      var y = (0.2126 * r + 0.7152 * g + 0.0722 * b).toInt();
+      var newColor = y;
+      newColor += (y >> 8);
+      newColor += (y >> 16);
+      img.setPixel(j,i, newColor)
     }
   }
 
   return (white.toDouble() / total.toDouble());
+}
+
+imglib.Image getBlackAndWhiteImage(imglib.Image image ) {
+  var bht = getBht(getGrayScaleHistogram(image));
+
+}
+
+List<int> getGrayScaleHistogram(imglib.Image image) {
+  List<int> histogram = List(255);
+  for (int i = 0; i < histogram.length; i++) {
+    histogram[i] = 0;
+  }
+
+  for (int i = 0; i < image.height; i++) {
+    for (int j = 0; j < image.width; j++) {
+      histogram[image.getPixel(j, i) & 0xFF]++;
+    }
+  }
+
+  return histogram;
+}
+
+int getBht(List<int> histogram, {int minCount = 5}) {
+  var start = 0;
+  while (histogram[start] < minCount) {
+    start++;
+  }
+
+  var end = histogram[histogram.length - 1];
+  while (histogram[end] < minCount) {
+    end--;
+  }
+
+  //     h_c = int(round(np.average(np.linspace(0, 2 ** 8 - 1, n_bins), weights=hist)))
+  int center = ((start + end) / 2).floor();
+
+  int weightLeft = histogram.getRange(start, center).reduce((a, b) => a + b);
+  int weightRight = histogram.getRange(center, end + 1).reduce((a, b) => a + b);
+
+  while (start < end) {
+    if (weightLeft > weightRight) {
+      weightLeft -= histogram[start];
+      start++;
+    } else {
+      weightRight -= histogram[end];
+      end--;
+    }
+
+    int newCenter = ((start + end) / 2).floor();
+
+    if (newCenter < center) {
+      weightLeft -= histogram[center];
+      weightRight += histogram[center];
+    } else if (newCenter > center) {
+      weightLeft += histogram[center];
+      weightRight -= histogram[center];
+    }
+
+    center = newCenter;
+  }
+
+  return center;
 }
