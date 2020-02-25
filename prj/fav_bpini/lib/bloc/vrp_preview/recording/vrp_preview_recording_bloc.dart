642903c576d39a6fdfe45a08eb814fb49bf504a8
Stanislav Král <stanislav.kral3@gmail.com>
2020-02-25 13:39:00 +0100

    Fixed voice note recorder

--- prj/fav_bpini/lib/bloc/vrp_preview/recording/vrp_preview_recording_bloc.dart
+++ prj/fav_bpini/lib/bloc/vrp_preview/recording/vrp_preview_recording_bloc.dart
@@ -1,61 +1,69 @@
 import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:favbpini/bloc/vrp_preview/recording/vrp_preview_recording_event.dart';
 import 'package:favbpini/bloc/vrp_preview/recording/vrp_preview_recording_state.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class VrpPreviewRecordingBloc extends Bloc<VrpPreviewRecordingEvent, VrpPreviewRecordingState> {
   final _sound = FlutterSound();
   var audioPath;
   var recorded = false;
 
+  StreamSubscription _recorderSubscription;
+  StreamSubscription _playerSubscription;
+
   @override
   VrpPreviewRecordingState get initialState => InitialVrpPreviewRecordingState();
 
   @override
-  Stream<VrpPreviewRecordingState> mapEventToState(VrpPreviewRecordingEvent event,) async* {
+  Stream<VrpPreviewRecordingState> mapEventToState(
+    VrpPreviewRecordingEvent event,
+  ) async* {
     debugPrint("event is ");
     if (event is RecordingStarted) {
-      debugPrint("requesting permission");
-      Map<PermissionGroup, PermissionStatus> permissions = await PermissionHandler().requestPermissions([PermissionGroup.microphone]);
-      debugPrint("requested permission");
-      PermissionStatus permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.microphone);
-      debugPrint("got permission status");
+      debugPrint("Received recording started event");
+//      debugPrint("requesting permission");
+//      Map<PermissionGroup, PermissionStatus> permissions = await PermissionHandler().requestPermissions([PermissionGroup.microphone]);
+//      debugPrint("requested permission");
+//      PermissionStatus permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.microphone);
+//      debugPrint("got permission status");
 
-      if (permission == PermissionStatus.granted) {
-        debugPrint("Mic permission granted");
-        yield RecordingInProgress(DateTime.fromMillisecondsSinceEpoch(0));
-        final dir = await getTemporaryDirectory();
-        debugPrint("Starting recording...");
-        audioPath = "$dir/${DateTime
-            .now()
-            .millisecondsSinceEpoch}.audio";
-        try {
-          await _sound.startRecorder(uri: audioPath);
+//      if (permission == PermissionStatus.granted) {
+      debugPrint("Mic permission granted");
+      yield RecordingInProgress(DateTime.fromMillisecondsSinceEpoch(0));
+      final dir = await getTemporaryDirectory();
+      debugPrint("Starting recording...");
+      audioPath = "${dir.path}/${DateTime.now().millisecondsSinceEpoch}.audio";
 
-          debugPrint("Starterd recording");
+      debugPrint("The path to audio is: $audioPath");
+      try {
+        await _sound.startRecorder(uri: audioPath);
 
-          _sound.onRecorderStateChanged.listen((event) {
-            add(RecordingUpdated(event.currentPosition.toInt()));
-          });
-        } catch (exception) {
-          debugPrint("Exception while starting recorder: " + exception);
-          yield RecordingFailed("Nepodařilo se spustit nahrávání.");
-        }
-      } else {
-        debugPrint("No permissions granted");
-        yield RecordingFailed("Bez povolení nelze nahrávat zvuk z mikrofonu.");
+        debugPrint("Starterd recording");
+
+        _recorderSubscription = _sound.onRecorderStateChanged.listen((event) {
+          add(RecordingUpdated(event.currentPosition.toInt()));
+        });
+      } catch (exception) {
+        debugPrint("Exception while starting recorder: " + exception);
+        yield RecordingFailed("Nepodařilo se spustit nahrávání.");
       }
+//      } else {
+//        debugPrint("No permissions granted");
+//        yield RecordingFailed("Bez povolení nelze nahrávat zvuk z mikrofonu.");
+//      }
     } else if (event is RecordingUpdated) {
       yield RecordingInProgress(DateTime.fromMillisecondsSinceEpoch(event.currentPosition.toInt()));
     } else if (event is RecordingStopped) {
+      debugPrint("RecordingStopped event");
       try {
+        _recorderSubscription.cancel();
         _sound.stopRecorder();
         recorded = true;
-        yield InitialVrpPreviewRecordingState();
+        yield RecordingSuccess(audioPath);
       } catch (exception) {
         recorded = false;
         debugPrint("Exception while stopping recorder: " + exception);
@@ -65,8 +73,12 @@ class VrpPreviewRecordingBloc extends Bloc<VrpPreviewRecordingEvent, VrpPreviewR
       try {
         await _sound.startPlayer(audioPath);
 
-        _sound.onPlayerStateChanged.listen((event) {
-          add(PlaybackUpdated(event.currentPosition.toInt()));
+        _playerSubscription = _sound.onPlayerStateChanged.listen((event) {
+          if (event != null) {
+            add(PlaybackUpdated(event.currentPosition.toInt()));
+          } else {
+            add(PlaybackStopped());
+          }
         });
       } catch (exception) {
         debugPrint("Exception while starting player: " + exception);
@@ -75,12 +87,22 @@ class VrpPreviewRecordingBloc extends Bloc<VrpPreviewRecordingEvent, VrpPreviewR
     } else if (event is PlaybackUpdated) {
       yield PlaybackInProgress(DateTime.fromMillisecondsSinceEpoch(event.currentPosition.toInt()));
     } else if (event is PlaybackStopped) {
+      debugPrint("PlaybackStopped event");
       try {
+        _playerSubscription.cancel();
         _sound.stopPlayer();
+        yield RecordingSuccess(audioPath);
       } catch (exception) {
         debugPrint("Exception while stopping player: " + exception);
         yield RecordingFailed("Nepodařilo se zastavit nahrávání.");
       }
     }
+  }
+
+  @override
+  Future<Function> close() {
+    _sound.stopPlayer();
+    _sound.stopRecorder();
+    return this.close();
   }
 }
